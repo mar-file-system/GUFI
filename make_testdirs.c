5d1ec92eabd9ef47b57c62944373ca0230e7c455
Jeff Inman <jti@lanl.gov>
2017-11-03 10:46:27 -0600

    Fixed options-parsing in make_testdirs.

--- make_testdirs.c
+++ make_testdirs.c
@@ -7,13 +7,24 @@
 #include <string.h>
 #include <errno.h>
 
+// Usage:
+// 
+//    make_testdirs -d <n_dirs> -f <n_files> dirname
+//
+// create <n_dirs> directories directly under <dirname>.  Each directory
+// contains <n_files> files.  Each file has a sparse file, with size in the
+// range [0, 64k).
+//
+// <n_files> and <n_dirs> both default to 1024.
+
+
 #define MAX_PATHNAME  256
 
 void usage(const char* progname) {
   printf("Usage: %s [ options ] dirname\n", progname);
   printf("options:\n");
-  printf("  -d <n_dirs>    n subdirs\n");
-  printf("  -f <n_files>   n files per subdir\n");
+  printf("  -d <n_dirs>    number of subdirs\n");
+  printf("  -f <n_files>   number of files per subdir\n");
   printf("  -h             help\n");
 }
 
@@ -70,52 +81,72 @@ int make_it(const char* dir,
 
 int main (int argc, char* argv[]) {
 
-  unsigned n_dirs  = 1024;        // default
-  unsigned n_files = 1024;        // default
-  int      help    = 0;
-  char*    dir     = NULL;
+   unsigned n_dirs  = 1024;        // default
+   unsigned n_files = 1024;        // default
+   int      ch;
+   int      help    = 0;
+   int      err     = 0;
+   char*    dir     = NULL;
 
-  int   c;
-  optind = 0;
+# define PARSE_INT(VAR, STR)                       \
+   do {                                            \
+      errno = 0;                                   \
+      VAR = strtol((STR), NULL, 10);               \
+      if (errno) {                                 \
+         printf("PARSE_INT(" #VAR ") failed\n");   \
+         err = 1;                                  \
+      }                                            \
+   } while (0)
+        
+  
+   while ((! err)
+          && (ch = getopt(argc, argv, "hd:f:")) != -1) {
 
-  while ((c = getopt(argc, argv, "hd:f:")) != -1) {
+      switch (ch) {
 
-    printf("-- option '%c'\n", c);
-    printf("\t optarg:      '%s'\n", optarg);
-    printf("\t optind:      %d\n", optind);
+      case 'd':  PARSE_INT(n_dirs, optarg);       break;
+      case 'f':  PARSE_INT(n_files, optarg);       break;
+      case 'h':  help = 1; break;
 
-    switch (c) {
+      case '?':
+         // getopt returns '?' when there is a problem.
+         printf ("\t unrecognized option '%s'\n", argv[optind -1]);
+         err = 1;
+         break;
 
-    case 'd':  n_dirs = atoi(optarg);        break;
-    case 'f':  n_files = atoi(optarg);       break;
-    case 'h':  help = 1; break;
+      default:
+         printf ("\t ?? getopt returned '%c' 0%o ??\n", (char)ch, ch);
+         err = 1;
+      }
+   }
 
-    case '?':
-      // getopt returns '?' when there is a problem.
-      printf ("\t unrecognized option '%s'\n", argv[optind -1]);
-      break;
 
-    default:
-      printf ("\t ?? getopt returned '%c' 0%o ??\n", (char)c, c);
-    }
-  }
+   if (help) {
+      usage(argv[0]);
+      exit(0);
+   }
+   if (err) {
+      usage(argv[0]);
+      exit(-1);
+   }
+   if (argc == 1) {
+      usage(argv[0]);
+      exit(-1);
+   }
+   if (optind >= argc) {
+      usage(argv[0]);
+      exit(-1);
+   }
 
-  if (help) {
-    usage(argv[0]);
-    exit(0);
-  }
+   dir = argv[optind++];
+  
 
-  if (optind >= argc) {
-    usage(argv[0]);
-    exit(-1);
-  }
-  dir = argv[optind++];
 
-  unsigned seed = 0;
-  srand(seed);
+   unsigned seed = 0;
+   srand(seed);
 
-  mkdir(dir, 0777);
-  make_it(dir, n_dirs, n_files);
+   mkdir(dir, 0777);
+   make_it(dir, n_dirs, n_files);
 
-  return 0;
+   return 0;
 }
