e0728af415cfa26a52a8713d6a4dd0c44e7376dc
Jason Lee <jasonlee@lanl.gov>
2019-01-24 11:49:31 -0700

    CMake fixes

--- dbutils.c
+++ dbutils.c
@@ -86,6 +86,8 @@ OF SUCH DAMAGE.
 
 #include "bf.h"
 
+const char PCRE[] = "sqlite3-pcre/lib/libpcre";
+
 char *rsql = // "DROP TABLE IF EXISTS readdirplus;"
             "CREATE TABLE readdirplus(path TEXT, type TEXT, inode INT64 PRIMARY KEY, pinode INT64, suspect INT64);";
 
@@ -258,7 +260,7 @@ exit(9);
 
     // Load a regular expression extension
     if ((sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_LOAD_EXTENSION, 1, NULL) != SQLITE_OK) ||
-        (sqlite3_load_extension(db, "sqlite3-pcre/lib/libpcre.so", NULL, NULL) != SQLITE_OK)) {
+        (sqlite3_load_extension(db, PCRE, NULL, NULL) != SQLITE_OK)) {
         fprintf(stderr, "Unable to load regex extension\n");
         sqlite3_close(db);
         db = NULL;
@@ -944,7 +946,7 @@ sqlite3 *open_aggregate(const char *name, const char *attach_name, const char *q
     char *err_msg = NULL;
     if ((sqlite3_open_v2(name, &aggregate, SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE | SQLITE_OPEN_URI, NULL) != SQLITE_OK) || // create the aggregate database
         (sqlite3_db_config(aggregate, SQLITE_DBCONFIG_ENABLE_LOAD_EXTENSION, 1, NULL)                          != SQLITE_OK) || // enable extension loading
-        (sqlite3_load_extension(aggregate, "sqlite3-pcre/pcre.so", NULL, NULL)                                 != SQLITE_OK) || // load sqlite3-pcre
+        (sqlite3_load_extension(aggregate, PCRE, NULL, &err_msg)                                               != SQLITE_OK) || // load sqlite3-pcre
         (addqueryfuncs(aggregate)                                                                              != 0)         || // this is needed to add some query functions like path() uidtouser() gidtogroup()
         (sqlite3_exec(aggregate, esql, NULL, NULL, &err_msg)                                                   != SQLITE_OK) || // create the original entries table for the aggregate table to copy from
         (sqlite3_exec(aggregate, create_results_table, NULL, NULL, &err_msg)                                   != SQLITE_OK) || // create the aggregate table
