eb7e6a61a1cf5aad574ebd9df1f41c380484c95e
Jason Lee <jasonlee@lanl.gov>
2019-05-14 03:50:36 -0600

    drop caches before running canned queries
    also use 16 threads instead of 2

--- contrib/benchmark.sh
+++ contrib/benchmark.sh
@@ -14,7 +14,7 @@ fi
 
 GUFI_PREFIX="$(realpath $1)"
 GUFI_TREE="$(realpath $2)"
-THREADS="2"
+THREADS="16"
 BFQ=src/bfq
 
 if [[ "$#" -gt 2 ]]; then
@@ -26,18 +26,22 @@ COMMON="--threads ${THREADS} --bfq ${BFQ}"
 
 echo
 echo "Top 10 Users taking up the most space"
+echo 3 > /proc/sys/vm/drop_caches
 sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats ${COMMON} --order most --num_results 10 space ${GUFI_TREE}
 
 echo
 echo "Total Number of Files"
+echo 3 > /proc/sys/vm/drop_caches
 sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats ${COMMON} total-files ${GUFI_TREE}
 
 echo
 echo "Total Space Taken"
+echo 3 > /proc/sys/vm/drop_caches
 sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats ${COMMON} total-space ${GUFI_TREE}
 
 echo
 echo "Top 10 Users with the most files"
+echo 3 > /proc/sys/vm/drop_caches
 MOST_FILES="$(sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats ${COMMON} --order most --num_results 10 files ${GUFI_TREE})"
 echo "${MOST_FILES}"
 
@@ -45,12 +49,15 @@ MOST_FILES="$(echo "${MOST_FILES}" | head -n 1 | awk -F'|' '{printf $1}')"
 
 echo
 echo "Number of files owned by user ${MOST_FILES} (gufi_find)"
+echo 3 > /proc/sys/vm/drop_caches
 sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find ${COMMON} --no-aggregate ${GUFI_TREE} | wc -l
 
 echo
 echo "Number of files owned by user ${MOST_FILES} (gufi_stats)"
+echo 3 > /proc/sys/vm/drop_caches
 sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_stats ${COMMON} total-files ${GUFI_TREE}
 
 echo
 echo "Number of files owned by user ${MOST_FILES} larger than 1KB (gufi_find)"
+echo 3 > /proc/sys/vm/drop_caches
 sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find ${COMMON} --no-aggregate -size=+1024c ${GUFI_TREE} | wc -l
