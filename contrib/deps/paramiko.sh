85da91196a81755e77523b1854c1c337508ca7bf
Jason Lee <jasonlee@lanl.gov>
2019-06-12 12:39:00 -0600

    paramiko update

--- contrib/deps/paramiko.sh
+++ contrib/deps/paramiko.sh
@@ -8,16 +8,16 @@ set -e
 paramiko_name="paramiko"
 paramiko_prefix="${INSTALL_DIR}/${paramiko_name}"
 if [[ ! -d "${paramiko_prefix}" ]]; then
-    paramiko_build="${BUILD_DIR}/paramiko-cf7d49d66f3b1fbc8b0853518a54050182b3b5eb"
+    paramiko_build="${BUILD_DIR}/paramiko-master"
     if [[ ! -d "${paramiko_build}" ]]; then
         paramiko_tarball="${DOWNLOAD_DIR}/paramiko.tar.gz"
         if [[ ! -f "${paramiko_tarball}" ]]; then
-            wget https://github.com/paramiko/paramiko/archive/cf7d49d66f3b1fbc8b0853518a54050182b3b5eb.tar.gz -O "${paramiko_tarball}"
+            wget https://github.com/paramiko/paramiko/archive/master.tar.gz -O "${paramiko_tarball}"
         fi
     fi
 
     if [[ ! -d "${paramiko_build}" ]]; then
         tar -xf "${paramiko_tarball}" -C "${BUILD_DIR}"
-        patch -d "${paramiko_build}" -p1 < "${SCRIPT_PATH}/paramiko-cf7d49d-hostbased.patch"
+        patch -d "${paramiko_build}" -p1 < "${SCRIPT_PATH}/paramiko.patch"
     fi
 fi
