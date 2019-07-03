965242fd26dddeb711730cf03a946c5f250682ce
Jason Lee <jasonlee@lanl.gov>
2019-07-03 05:50:56 -0600

    paramiko dependencies
    Added install script to install with pip
    Added install instruction to install through wheel files
    python2-pip installed through rpm is now required by the GUFI-Client RPM
    
    [ci skip]

---
+++ contrib/python/pip_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# This script installs python-cryptography,
+# python-bcrypt, and python-pynacl with pip.
+
+set -e
+
+# check if running as root
+USER=""
+if [[ "$(id -u)" -ne 0 ]]
+then
+    USER="--user"
+fi
+
+pip install ${USER} cryptography bcrypt pynacl
