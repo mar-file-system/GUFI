6b1a15ef611bf1afe433eb6c6c667fb89ca088ae
Jason Lee <jasonlee@lanl.gov>
2019-08-07 08:58:03 -0600

    gentrace.py fix
    files were being placed in the wrong directories

--- contrib/gentrace.py
+++ contrib/gentrace.py
@@ -3,18 +3,19 @@
 import argparse
 import sys
 
-rs = chr(0x1e);
+rs = chr(0x1e)
 
 def generate_level(out, parent, dir_count, file_count, current_level, max_level):
-    if current_level == max_level:
-        return
+    global files, dirs
 
     for d_name in xrange(dir_count):
         out.write(parent + '/' + str(d_name) + '.d' + rs + 'd' + rs + '1' + rs + '16832' + rs + '1' + rs + '1' + rs + '1'  + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + rs + rs + rs + '1' + rs + '1' + rs +'\n')
+        dirs += 1
 
-        # generate all files first
-        for f_name in xrange(file_count):
-            out.write(parent + '/' + str(d_name) + '.d' + '/' + str(f_name) + '.f' + rs + 'f' + rs + '1' + rs + '33188' + rs + '1' + rs + '1' + rs + '1'  + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + rs + rs + rs + '1' + rs + '1' + rs + '\n')
+    # generate all files first
+    for f_name in xrange(file_count):
+        out.write(parent + '/' + str(f_name) + '.f' + rs + 'f' + rs + '1' + rs + '33188' + rs + '1' + rs + '1' + rs + '1'  + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + '1' + rs + rs + rs + rs + '1' + rs + '1' + rs + '\n')
+        files += 1
 
     for d_name in xrange(dir_count):
         # generate each directory with its children
@@ -28,4 +29,9 @@ if __name__=='__main__':
     parser.add_argument('depth',       type=int,                      help='number of levels')
     args = parser.parse_args()
 
-    generate_level(args.output, '', args.directories, args.files, 0, args.depth)
+    files = 0
+    dirs = 0
+
+    generate_level(args.output, '', args.directories, args.files, 1, args.depth)
+
+    print files, dirs
