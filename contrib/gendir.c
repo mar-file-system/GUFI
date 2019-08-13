e4d7b7c5aa48a1f3b14ce71f144517ad89049209
Jason Lee <jasonlee@lanl.gov>
2019-08-13 07:19:46 -0600

    use QueuePerThreadPool in gendir
    CMakeLists updates

--- contrib/gendir.c
+++ contrib/gendir.c
@@ -18,7 +18,7 @@ The number of directories there would be if each directory spawned 1 subdirector
 
 The number of directories generated is
 
-           # of directories * s
+           # of directories * s + 1
 
 The number of files generated is
 
@@ -32,68 +32,76 @@ The number of files generated is
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
+#include "QueuePerThreadPool.h"
+#include "utils.h"
+
 extern int errno;
 
-long double elapsed(const struct timespec *start, const struct timespec *end) {
-    const long double s = ((long double) start->tv_sec) + ((long double) start->tv_nsec) / 1000000000ULL;
-    const long double e = ((long double) end->tv_sec)   + ((long double) end->tv_nsec)   / 1000000000ULL;
-    return e - s;
-}
+#define DIR_PERMS (S_IRWXU | S_IRWXG | S_IRWXO)
 
-int generate_level(const char * dir, const size_t subdir_count, const size_t file_count, const size_t current_level, const size_t max_level) {
-    if (current_level >= max_level) {
+// constants to pass to threads
+struct settings {
+    size_t dirs;
+    size_t files;
+    size_t max_level;
+};
+
+// work for each thread
+struct dir {
+    char name[MAXPATH];
+    size_t current_level;
+};
+
+int generate_level(struct QPTPool * ctx, void * data, const size_t id, size_t * next_queue, void * args) {
+    struct settings * settings = (struct settings *) args;
+    struct dir * dir = (struct dir *) data;
+
+    if (dir->current_level >= settings->max_level) {
         return 0;
     }
 
-    char name[4096];
-    // create the files in this directory
-    for(size_t i = 0; i < file_count; i++) {
-        int rc = snprintf(name, 4096, "%s/f.%zu", dir, i);
-        if ((rc < 0) || (4096 <= rc)) {
-            return 1;
+    // create the subdirectories in this directory
+    for(size_t i = 0; i < settings->dirs; i++) {
+        struct dir * subdir = calloc(1, sizeof(struct dir));
+        if (!subdir) {
+            continue;
         }
+
+        SNPRINTF(subdir->name, MAXPATH, "%s/d.%zu", dir, i);
+
+        // no need to recursively make upper directories because they have already been created
+        if (mkdir(subdir->name, DIR_PERMS) < 0) {
+            if (errno != EEXIST) {
+                fprintf(stderr, "mkdir failed for %s: %d %s\n", subdir->name, errno, strerror(errno));
+                free(subdir);
+                continue;
+            }
+        }
+
+        subdir->current_level = dir->current_level + 1;
+
+        // recurse down by placing subdirectories onto queue
+        QPTPool_enqueue_internal(ctx, subdir, next_queue);
+    }
+
+    // create the files in this directory
+    char name[MAXPATH];
+    for(size_t i = 0; i < settings->files; i++) {
+        SNPRINTF(name, MAXPATH, "%s/f.%zu", dir->name, i);
 
         // overwrite any existing data
         const int fd = open(name, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
         if (fd < 0) {
             fprintf(stderr, "open failed for %s: %d %s\n", name, errno, strerror(errno));
-            return 1;
         }
         close(fd);
-    }
-
-    // create the subdirectories in this directory
-    for(size_t i = 0; i < subdir_count; i++) {
-        int rc = snprintf(name, 4096, "%s/d.%zu", dir, i);
-        if ((rc < 0) || (4096 <= rc)) {
-            return 1;
-        }
-
-        if (mkdir(name, S_IRWXU | S_IRWXG | S_IRWXO) < 0) {
-            if (errno != EEXIST) {
-                fprintf(stderr, "mkdir failed for %s: %d %s\n", name, errno, strerror(errno));
-                return 1;
-            }
-        }
-    }
-
-    // recurse down each subdirectory
-    for(size_t i = 0; i < subdir_count; i++) {
-        int rc = snprintf(name, 4096, "%s/d.%zu", dir, i);
-        if ((rc < 0) || (4096 <= rc)) {
-            return 1;
-        }
-
-        if (generate_level(name, subdir_count, file_count, current_level + 1, max_level) != 0) {
-            fprintf(stderr, "descent failed for %s\n", name);
-            return 1;
-        }
     }
 
     return 0;
@@ -101,61 +109,88 @@ int generate_level(const char * dir, const size_t subdir_count, const size_t fil
 
 int main(int argc, char * argv[]) {
     if (argc < 5) {
-        fprintf(stderr, "Syntax: %s output_dir directories files depth\n", argv[0]);
+        fprintf(stderr, "Syntax: %s output_dir directories files depth [threads=1]\n", argv[0]);
         return 1;
     }
 
-    const char * output = argv[1];
+    struct settings settings;
 
-    size_t dirs = 0;
-    if ((sscanf(argv[2], "%zu", &dirs) != 1) || (dirs < 1)) {
+    if (sscanf(argv[2], "%zu", &settings.dirs) != 1) {
         fprintf(stderr, "Bad directory count: %s\n", argv[2]);
         return 1;
     }
 
-    size_t files = 0;
-    if (sscanf(argv[3], "%zu", &files) != 1) {
+    if (sscanf(argv[3], "%zu", &settings.files) != 1) {
         fprintf(stderr, "Bad file_count count: %s\n", argv[3]);
         return 1;
     }
 
-    size_t depth = 0;
-    if ((sscanf(argv[4], "%zu", &depth) != 1) || (depth < 1)) {
+    if (sscanf(argv[4], "%zu", &settings.max_level) != 1) {
         fprintf(stderr, "Bad depth: %s\n", argv[4]);
         return 1;
     }
 
-    printf("Generating into %s: %zu levels, %zu directories per level, %zu files per directory\n", output, depth, dirs, files);
-
-    if (mkdir(output, S_IRWXU | S_IRWXG | S_IRWXO) < 0) {
-        if (errno != EEXIST) {
-            fprintf(stderr, "mkdir failed for %s: %d %s\n", output, errno, strerror(errno));
+    size_t threads = 1;
+    if (argc > 5) {
+        if (sscanf(argv[5], "%zu", &threads) != 1) {
+            fprintf(stderr, "Bad thread count: %s\n", argv[5]);
             return 1;
         }
     }
 
-    size_t sum = 1;
-    for(size_t i = 0; i < depth; i++) {
-        sum *= dirs;
-    }
-    sum = (sum - 1) / (dirs - 1);
+    printf("Generating into %s with %zu threads: %zu levels, %zu directories per level, %zu files per directory\n", argv[1], threads, settings.max_level, settings.dirs, settings.files);
 
-    const size_t total_dirs = dirs * sum;
-    const size_t total_files = files * sum;
+    size_t sum = 1;
+    for(size_t i = 0; i < settings.max_level; i++) {
+        sum *= settings.dirs;
+    }
+    sum = (sum - 1) / (settings.dirs - 1);
+
+    const size_t total_dirs = settings.dirs * sum;
+    const size_t total_files = settings.files * sum;
 
     printf("Total Dirs:            %zu\n", total_dirs);
     printf("Total Files:           %zu\n", total_files);
 
     struct timespec start;
     clock_gettime(CLOCK_MONOTONIC, &start);
-    const int rc = generate_level(output, dirs, files, 0, depth);
-    struct timespec end;
-    clock_gettime(CLOCK_MONOTONIC, &end);
 
-    if (rc != 0) {
-        fprintf(stderr, "Error generating tree\n");
+    // generate the top level
+    // each thread only generates the contents of its directory, not the directory itself
+    struct dir * root = calloc(1, sizeof(struct dir));
+    if (!root) {
+        fprintf(stderr, "Could not allocate root work: %s\n", argv[2]);
         return 1;
     }
+    memcpy(root->name, argv[1], strlen(argv[1]));
+
+    struct stat top_st;
+    top_st.st_mode = DIR_PERMS;
+    top_st.st_uid = geteuid();
+    top_st.st_uid = getegid();
+    if (dupdir(root->name, &top_st) != 0) {
+        fprintf(stderr, "mkdir failed for %s: %d %s\n", root->name, errno, strerror(errno));
+        return 1;
+    }
+
+    // start up threads and push root into the queue for processing
+    struct QPTPool * pool = QPTPool_init(threads);
+    if (!pool) {
+        fprintf(stderr, "Failed to initialize thread pool\n");
+        return -1;
+    }
+
+    QPTPool_enqueue_external(pool, root);
+    if (QPTPool_start(pool, generate_level, &settings) != threads) {
+        fprintf(stderr, "Failed to start all threads\n");
+        return -1;
+    }
+
+    QPTPool_wait(pool);
+    QPTPool_destroy(pool);
+
+    struct timespec end;
+    clock_gettime(CLOCK_MONOTONIC, &end);
 
     const long double gen_time = elapsed(&start, &end);
 
