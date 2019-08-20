64a511d53c325b53e5d55b6858ce73b8835d50f9
Jason Lee <jasonlee@lanl.gov>
2019-08-20 03:12:45 -0600

    Updated benchmark script
    use gufi_query instead of bfq
    --no-aggregate was removed
    don't pin processes to numa node 5

--- contrib/benchmark.sh
+++ contrib/benchmark.sh
@@ -15,14 +15,14 @@ fi
 GUFI_PREFIX="$(realpath $1)"
 GUFI_TREE="$(realpath $2)"
 THREADS="16"
-BFQ=src/bfq
+BFQ=src/gufi_query
 
 if [[ "$#" -gt 2 ]]; then
     THREADS="$3"
 fi
 
-NUMACTL="numactl -m 5 -N 5"
-COMMON="--threads ${THREADS} --bfq ${BFQ}"
+NUMACTL= # "numactl -m 5 -N 5"
+COMMON="--threads ${THREADS} --exec ${BFQ}"
 
 echo
 echo "Top 10 Users taking up the most space"
@@ -50,7 +50,7 @@ MOST_FILES="$(echo "${MOST_FILES}" | head -n 1 | awk -F'|' '{printf $1}')"
 echo
 echo "Number of files owned by user ${MOST_FILES} (gufi_find)"
 echo 3 > /proc/sys/vm/drop_caches
-sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find ${COMMON} --no-aggregate ${GUFI_TREE} | wc -l
+sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find ${COMMON} ${GUFI_TREE} | wc -l
 
 echo
 echo "Number of files owned by user ${MOST_FILES} (gufi_stats)"
@@ -60,4 +60,4 @@ sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_stats ${COMMON} total-f
 echo
 echo "Number of files owned by user ${MOST_FILES} larger than 1KB (gufi_find)"
 echo 3 > /proc/sys/vm/drop_caches
-sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find ${COMMON} --no-aggregate -size=+1024c ${GUFI_TREE} | wc -l
+sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find ${COMMON} -size=+1024c ${GUFI_TREE} | wc -l
