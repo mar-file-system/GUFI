bf46fdf042049a045ac1693432f91c3b16f6f201
Jason Lee <jasonlee@lanl.gov>
2019-11-19 11:59:51 -0700

    canned_queries updates
    slightly different results from before, since now files and links are now treated differently

--- contrib/canned_queries.sh
+++ contrib/canned_queries.sh
@@ -14,27 +14,25 @@ fi
 
 GUFI_PREFIX="$(realpath $1)"
 
-NUMACTL= # "numactl -m 5 -N 5"
-
 echo
 echo "Top 10 Users taking up the most space"
 echo 3 > /proc/sys/vm/drop_caches
-sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats --order most --num_results 10 space
+sudo ${GUFI_PREFIX}/gufi_stats --order most --num_results 10 space
 
 echo
 echo "Total Number of Files"
 echo 3 > /proc/sys/vm/drop_caches
-sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats total-files
+sudo ${GUFI_PREFIX}/gufi_stats total-files
 
 echo
 echo "Total Space Taken"
 echo 3 > /proc/sys/vm/drop_caches
-sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats total-space
+sudo ${GUFI_PREFIX}/gufi_stats total-space
 
 echo
 echo "Top 10 Users with the most files"
 echo 3 > /proc/sys/vm/drop_caches
-MOST_FILES="$(sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats --order most --num_results 10 files)"
+MOST_FILES="$(sudo ${GUFI_PREFIX}/gufi_stats --order most --num_results 10 files)"
 echo "${MOST_FILES}"
 
 MOST_FILES="$(echo "${MOST_FILES}" | head -n 1 | awk -F'|' '{printf $1}')"
@@ -42,14 +40,14 @@ MOST_FILES="$(echo "${MOST_FILES}" | head -n 1 | awk -F'|' '{printf $1}')"
 echo
 echo "Number of files owned by user ${MOST_FILES} (gufi_find)"
 echo 3 > /proc/sys/vm/drop_caches
-sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find | wc -l
+sudo -u \#"${MOST_FILES}" ${GUFI_PREFIX}/gufi_find -type f | wc -l
 
 echo
 echo "Number of files owned by user ${MOST_FILES} (gufi_stats)"
 echo 3 > /proc/sys/vm/drop_caches
-sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_stats total-files
+sudo -u \#"${MOST_FILES}" ${GUFI_PREFIX}/gufi_stats total-files
 
 echo
 echo "Number of files owned by user ${MOST_FILES} larger than 1KB (gufi_find)"
 echo 3 > /proc/sys/vm/drop_caches
-sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find -size=+1024c | wc -l
+sudo -u \#"${MOST_FILES}" ${GUFI_PREFIX}/gufi_find -type f -size=+1024c | wc -l
