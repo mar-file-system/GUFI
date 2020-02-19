ee476779c9fa8111091d09e48bd6cdd2f3a19500
Jason Lee <jasonlee@lanl.gov>
2020-02-19 09:32:19 -0700

    canned queries update

--- contrib/canned_queries.sh
+++ contrib/canned_queries.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # This file is part of GUFI, which is part of MarFS, which is released
 # under the BSD license.
 #
@@ -59,9 +60,6 @@
 # OF SUCH DAMAGE.
 
 
-
-#!/usr/bin/env bash
-
 set -e
 
 if [[ "$#" -lt 1 ]]; then
@@ -76,27 +74,25 @@ fi
 
 GUFI_PREFIX="$(realpath $1)"
 
-NUMACTL= # "numactl -m 5 -N 5"
-
 echo
 echo "Top 10 Users taking up the most space"
 echo 3 > /proc/sys/vm/drop_caches
-sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats --order most --num_results 10 space ${GUFI_TREE}
+sudo ${GUFI_PREFIX}/gufi_stats --order most --num_results 10 space
 
 echo
 echo "Total Number of Files"
 echo 3 > /proc/sys/vm/drop_caches
-sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats total-files ${GUFI_TREE}
+sudo ${GUFI_PREFIX}/gufi_stats total-files
 
 echo
 echo "Total Space Taken"
 echo 3 > /proc/sys/vm/drop_caches
-sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats total-space ${GUFI_TREE}
+sudo ${GUFI_PREFIX}/gufi_stats total-space
 
 echo
 echo "Top 10 Users with the most files"
 echo 3 > /proc/sys/vm/drop_caches
-MOST_FILES="$(sudo ${NUMACTL} ${GUFI_PREFIX}/gufi_stats --order most --num_results 10 files ${GUFI_TREE})"
+MOST_FILES="$(sudo ${GUFI_PREFIX}/gufi_stats --order most --num_results 10 files)"
 echo "${MOST_FILES}"
 
 MOST_FILES="$(echo "${MOST_FILES}" | head -n 1 | awk -F'|' '{printf $1}')"
@@ -104,14 +100,14 @@ MOST_FILES="$(echo "${MOST_FILES}" | head -n 1 | awk -F'|' '{printf $1}')"
 echo
 echo "Number of files owned by user ${MOST_FILES} (gufi_find)"
 echo 3 > /proc/sys/vm/drop_caches
-sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find ${GUFI_TREE} | wc -l
+sudo -u \#"${MOST_FILES}" ${GUFI_PREFIX}/gufi_find -type f | wc -l
 
 echo
 echo "Number of files owned by user ${MOST_FILES} (gufi_stats)"
 echo 3 > /proc/sys/vm/drop_caches
-sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_stats total-files ${GUFI_TREE}
+sudo -u \#"${MOST_FILES}" ${GUFI_PREFIX}/gufi_stats total-files
 
 echo
 echo "Number of files owned by user ${MOST_FILES} larger than 1KB (gufi_find)"
 echo 3 > /proc/sys/vm/drop_caches
-sudo -u \#"${MOST_FILES}" ${NUMACTL} ${GUFI_PREFIX}/gufi_find -size=+1024c ${GUFI_TREE} | wc -l
+sudo -u \#"${MOST_FILES}" ${GUFI_PREFIX}/gufi_find -type f -size=+1024c | wc -l
