4bb7178ae9025e156cce1328d75949193ed7311a
Jason Lee <jasonlee@lanl.gov>
2020-03-02 17:02:56 -0700

    update timestamp ploting script to use sqlsum and sqlent
    also thread count is gotten from the data, not from argv[1]

--- contrib/debug/plot_query.sh
+++ contrib/debug/plot_query.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "$#" -lt 2 ]]
+if [[ "$#" -lt 1 ]]
 then
-    echo "Syntax: $0 threads file"
+    echo "Syntax: $0 file"
     exit 1
 fi
 
-threads="$1"
-file="$2"
+file="$1"
 
 names="
 addqueryfuncs
@@ -33,7 +32,8 @@ readdir_branch
 set
 set_pragmas
 snprintf
-sqlite3_exec
+sqlsum
+sqlent
 sqlite3_open_v2
 strncmp
 strncmp_branch
@@ -54,34 +54,39 @@ while_branch
 within_descend
 "
 
-for name in ${names}
-do
-    grep -E "[0-9]+ ${name} [0-9]+ [0-9]+" "${file}" > "${file}.${name}" &
-done
+# for name in ${names}
+# do
+#     grep -E "[0-9]+ ${name} [0-9]+ [0-9]+" "${file}" > "${file}.${name}" &
+# done
+# wait
 
-wait
+thread_ids=$(awk '{ print $1 }' ${file}.wf | sort -n | uniq)
+lowest=$(echo "${thread_ids}" | head -n 1)
+highest=$(echo "${thread_ids}" | tail -n 1)
 
 gnuplot <<EOF &
 
-set terminal pngcairo color solid size 6400,3200 font ",32"
+set terminal pngcairo color solid size 12800,4800 font ",32"
 set output '${file}.png'
 set title "gufi_query Events"
 set xlabel "Seconds Since Arbitrary Epoch"
 set ylabel "Thread #"
 set key outside right
-set yrange [-1:${threads}]
+set yrange [${lowest}:${highest}]
 set ytics 5
 
-plot '${file}.wf'                using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 4 title 'wf', \
-     '${file}.wf_process_work'   using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'wf_process_work', \
-     '${file}.opendir'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'opendir', \
-     '${file}.opendb'            using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'opendb', \
-     '${file}.descend'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'descend', \
-     '${file}.sqlite3_exec'      using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'sqlite3_exec', \
-     '${file}.closedb'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'closedb', \
-     '${file}.closedir'          using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'closedir', \
-     '${file}.output_timestamps' using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'output_timestamps', \
-     '${file}.wf_next_work'      using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 linecolor 'blue' title 'wf_next_work', \
+plot '${file}.wf'                using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 16 title 'wf', \
+     '${file}.wf_process_work'   using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 8 title 'wf_process_work', \
+     '${file}.opendir'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'opendir', \
+     '${file}.opendb'            using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'opendb', \
+     '${file}.descend'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'descend', \
+     '${file}.sqlsum'            using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'sqlsum', \
+     '${file}.sqlent'            using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'sqlent', \
+     '${file}.closedb'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'closedb', \
+     '${file}.closedir'          using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'closedir', \
+     '${file}.output_timestamps' using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'output_timestamps', \
+     '${file}.wf_next_work'      using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 8 linecolor 'blue' title 'wf_next_work', \
+
 
 EOF
 
@@ -95,19 +100,20 @@ set ylabel "Thread #"
 set clip two
 set key outside right
 set xrange [1.1:1.15]
-set yrange [-1:${threads}]
+set yrange [${lowest}:${highest}]
 set ytics 5
 
-plot '${file}.wf'                using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 4 title 'wf', \
-     '${file}.wf_process_work'   using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'wf_process_work', \
-     '${file}.opendir'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'opendir', \
-     '${file}.opendb'            using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'opendb', \
-     '${file}.descend'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'descend', \
-     '${file}.sqlite3_exec'      using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'sqlite3_exec', \
-     '${file}.closedb'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'closedb', \
-     '${file}.closedir'          using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'closedir', \
-     '${file}.output_timestamps' using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 1 title 'output_timestamps', \
-     '${file}.wf_next_work'      using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 linecolor 'blue' title 'wf_next_work', \
+plot '${file}.wf'                using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 16 title 'wf', \
+     '${file}.wf_process_work'   using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 8 title 'wf_process_work', \
+     '${file}.opendir'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'opendir', \
+     '${file}.opendb'            using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'opendb', \
+     '${file}.descend'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'descend', \
+     '${file}.sqlsum'            using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'sqlsum', \
+     '${file}.sqlent'            using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'sqlent', \
+     '${file}.closedb'           using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'closedb', \
+     '${file}.closedir'          using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'closedir', \
+     '${file}.output_timestamps' using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 2 title 'output_timestamps', \
+     '${file}.wf_next_work'      using (\$3/1e9):1:(\$4-\$3)/1e9:(0) with vectors nohead filled linewidth 8 linecolor 'blue' title 'wf_next_work', \
 
 EOF
 
