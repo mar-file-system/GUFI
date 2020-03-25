74d40f6eebb0f17924b3676ee9de012c12ac3800
Jason Lee <jasonlee@lanl.gov>
2020-03-25 11:19:12 -0600

    added test to verify that a trace file's contents can be found in a GUFI tree
    updated verifytreeintrace to use QueuePerThreadPool

--- contrib/verifytreeintrace.cpp
+++ contrib/verifytreeintrace.cpp
@@ -63,30 +63,24 @@ OF SUCH DAMAGE.
 
 
 #include <atomic>
-#include <condition_variable>
 #include <cstring>
 #include <fstream>
 #include <iostream>
 #include <libgen.h>
-#include <list>
-#include <mutex>
 #include <string>
 #include <sstream>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <thread>
-#include <unistd.h>
 #include <vector>
 
 #include <sqlite3.h>
 
-#define MAXPATH 4096
-#define MAXXATTR 1024
-#define MAXSQL 4096
+#include "QueuePerThreadPool.h"
+#include "trace.h"
 
 // Data stored during first pass of input file
-struct first {
-    first()
+struct StanzaStart {
+    StanzaStart()
         : first_delim(std::string::npos),
           line(),
           offset(-1),
@@ -99,174 +93,56 @@ struct first {
     std::size_t entries;
 };
 
-typedef struct first * Row;
-
-Row new_row() {
-    return new struct first();
-}
-
-void delete_row(Row & row) {
-    delete row;
-}
-
-void parsefirst (const char delim, Row & work) {
-    work->first_delim = work->line.find(delim);
-}
-
-// basically thread args
-struct ThreadWork {
-    ThreadWork()
-        : id(0),
-          queue(),
-          mutex(),
-          cv()
-    {}
-
-    int id;
-    std::list <Row> queue;
-    std::mutex mutex;
-    std::condition_variable cv;
-};
-
-typedef std::pair <ThreadWork, std::thread> WorkPair;
-typedef std::vector <WorkPair> State;
-
-void scout_function(bool & correct, std::atomic_bool & scouting, const char * filename, State & consumers) {
-    // figure out whether or not this function is correct
-    std::ifstream file(filename, std::ios::binary);
-    if (!(scouting = (bool) file)) {
-        std::cerr << "Could not open file " << filename << std::endl;
-        return;
-    }
-
-    Row work = new_row();
-    if (!std::getline(file, work->line)) {
-        delete_row(work);
-        return;
-    }
-
-    parsefirst('\x1e', work);
-    work->offset = file.tellg();
-
-    int tid = 0;
-    std::size_t file_count = 0;
-    std::size_t dir_count = 1; // always start with a directory
-    std::size_t empty = 0;
-
-    std::string line;
-    while (correct && std::getline(file, line)) {
-        Row next = new_row();
-        next->line = std::move(line);
-
-        // parse
-        parsefirst('\x1e', next);
-
-        // push directories onto queues
-        if (next->line[next->first_delim + 1] == 'd') {
-            dir_count++;
-
-            empty += !work -> entries;
-            next->offset = file.tellg();
-
-            // put the previous work on the queue
-            {
-                std::lock_guard <std::mutex> lock(consumers[tid].first.mutex);
-                if (work -> entries) {
-                    consumers[tid].first.queue.emplace_back(std::move(work));
-                }
-                consumers[tid].first.cv.notify_all();
-            }
-
-            // round robin
-            tid++;
-            tid %= consumers.size();
-
-            work = next;
-        }
-        else {
-            work->entries++;
-            file_count++;
-            delete_row(next);
-        }
-    }
-
-    // insert the last work item
-    if (correct) {
-        std::lock_guard <std::mutex> lock(consumers[tid].first.mutex);
-        if (work -> entries) {
-            consumers[tid].first.queue.emplace_back(std::move(work));
-        }
-
-        consumers[tid].first.cv.notify_all();
-    }
-
-    scouting = false;
-
-    // release all condition variables
-    for(std::size_t i = 0; i < consumers.size(); i++) {
-        consumers[i].first.cv.notify_all();
-    }
-
-    std::cerr << "Done scouting" << std::endl
-              << "Files: " << file_count << std::endl
-              << "Dirs:  " << dir_count << " (" << empty << " empty)" << std::endl
-              << "Total: " << file_count + dir_count << std::endl;
-
-    return;
-}
-
-struct work {
-   char          name[MAXPATH];
-   char          type[2];
-   char          linkname[MAXPATH];
-   struct stat   statuso;
-   char          xattr[MAXXATTR];
-   int           crtime;
-};
-
-void parsetowork (char * delim, char * line, struct work * pinwork) {
-    char *p;
-    char *q;
-
-    //printf("in parsetowork delim %s line %s\n",delim,line);
-    line[strlen(line)-1]= '\0';
-    p=line;    q=strstr(p,delim); memset(q, 0, 1); snprintf(pinwork->name,MAXPATH,"%s",p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); snprintf(pinwork->type,2,"%s",p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_ino=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_mode=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_nlink=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_uid=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_gid=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_size=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_blksize=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_blocks=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_atime=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_mtime=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->statuso.st_ctime=atol(p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); snprintf(pinwork->linkname,MAXPATH,"%s",p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); snprintf(pinwork->xattr,MAXXATTR,"%s",p);
-    p=q+1;     q=strstr(p,delim); memset(q, 0, 1); pinwork->crtime=atol(p);
-}
-
 int count_callback(void * arg, int, char **, char **) {
     int * count = (int *) arg;
     (void) (*count)++;
     return 0;
 }
 
-bool check_stanza(Row & row, std::istream & stream, const std::string & tree, const char delim = '\x1e', const char dir = 'd') {
-    static char d[2] = {delim, '\0'};
+struct CheckStanzaArgs {
+    CheckStanzaArgs(const std::size_t threads,
+                    std::atomic_bool & correct,
+                    char * delim,
+                    const std::string & tree)
+        : threads(threads),
+          correct(correct),
+          delim(delim),
+          tree(tree),
+          traces()
+    {}
 
-    std::string line = row -> line;
-
-    const std::string::size_type first_delim = line.find(delim);
-    if (first_delim == std::string::npos) {
-        std::cerr << "Missing delimiter: " << line << std::endl;
-        return false;
+    ~CheckStanzaArgs() {
+        for(std::istream * trace : traces) {
+            delete trace;
+        }
     }
 
-    // assume that a directory was found
-    const std::string parent = tree + "/" + line.substr(0, first_delim - (line[first_delim - 1] == '/'));
+    const std::size_t threads;
+    std::atomic_bool & correct;
+    char * delim;
+    const std::string & tree;
+    std::vector <std::istream *> traces;
+};
+
+int check_stanza(struct QPTPool * ctx, const size_t id, void * data, void * args) {
+    struct CheckStanzaArgs * csa = static_cast <struct CheckStanzaArgs *> (args);
+    std::istream & trace = *(csa->traces[id]);
+
+    struct StanzaStart * sa = static_cast <struct StanzaStart *> (data);
+
+    // stop early
+    if (!csa->correct) {
+        delete sa;
+        return 0;
+    }
+
+    // assume that the current directory was found
+    std::string parent = csa->tree + "/" + sa->line.substr(0, sa->first_delim);
+
+    // remove trailing /
+    if (parent[parent.size() - 1] == '/') {
+        parent.resize(parent.size() - 1);
+    }
 
     // check the parent directory's properties
     {
@@ -274,37 +150,47 @@ bool check_stanza(Row & row, std::istream & stream, const std::string & tree, co
         struct stat st;
         if (stat(parent.c_str(), &st) != 0) {
             std::cerr << "Parent doesn't exist: " << parent << std::endl;
-            return false;
+            csa->correct = false;
+            delete sa;
+            return 1;
         }
 
         // make sure the path is a directory
         if (!S_ISDIR(st.st_mode)) {
             std::cerr << "Parent is not a directory: " << parent << std::endl;
-            return false;
+            csa->correct = false;
+            delete sa;
+            return 1;
         }
 
         // parse the line to check other values
         char buf[MAXPATH] = {};
-        memcpy(buf, line.c_str(), line.size());
+        memcpy(buf, sa->line.c_str(), sa->line.size());
         struct work work;
-        parsetowork(d, buf, &work);
+        linetowork(buf, csa->delim, &work);
 
         // make sure the permissions are correct
         if (st.st_mode != work.statuso.st_mode) {
             std::cerr << "Parent permission is incorrect:" << parent << std::endl;
-            return false;
+            csa->correct = false;
+            delete sa;
+            return 1;
         }
 
         // make sure the uid is correct
         if (st.st_uid != work.statuso.st_uid) {
             std::cerr << "Parent uid incorrect: " << parent << std::endl;
-            return false;
+            csa->correct = false;
+            delete sa;
+            return 1;
         }
 
         // make sure the gid is correct
         if (st.st_gid != work.statuso.st_gid) {
             std::cerr << "Parent gid incorrect: " << parent << std::endl;
-            return false;
+            csa->correct = false;
+            delete sa;
+            return 1;
         }
     }
 
@@ -313,30 +199,44 @@ bool check_stanza(Row & row, std::istream & stream, const std::string & tree, co
     // if the database path is too long, skip checking the entries
     if (db_name.size() > 490) {
         std::cerr << "Warning: Name too long. Skipping " << db_name << std::endl;
-        return true;
+        return 0;;
+    }
+
+    // open the database file
+    sqlite3 * db = nullptr;
+    if (sqlite3_open_v2(db_name.c_str(), &db, SQLITE_OPEN_READONLY, nullptr) != SQLITE_OK) {
+        std::cerr << "Could not open database file: " << db_name << std::endl;
+        csa->correct = false;
+        delete sa;
+        return 1;
     }
 
     // move the trace file to the starting non-directory
-    stream.seekg(row -> offset);
+    trace.seekg(sa->offset);
 
     // followed by a series of non-directories
+    std::size_t entries = 0;
+    int rc = 0;
     while (true) {
-        const std::istream::streampos pos = stream.tellg();
+        // store pos here so it can be jumped back to if this was a directory
+        const std::istream::streampos pos = trace.tellg();
 
-        if (!std::getline(stream, line)) {
+        std::string line;
+        if (!std::getline(trace, line)) {
             break;
         }
 
-        const std::string::size_type first_delim = line.find(delim);
-        if (first_delim == std::string::npos) {
+        const std::string::size_type first_delim = line.find(csa->delim[0]);
+        if (sa->first_delim == std::string::npos) {
             std::cerr << "Missing delimiter: " << line << std::endl;
-            return false;
+            rc = 1;
+            break;
         }
 
         // stop when a directory is encountered (new stanza)
-        if (line[first_delim + 1] == dir) {
+        if (line[first_delim + 1] == 'd') {
             // go back to the beginning of this line
-            stream.seekg(pos);
+            trace.seekg(pos);
             break;
         }
 
@@ -346,123 +246,176 @@ bool check_stanza(Row & row, std::istream & stream, const std::string & tree, co
         char buf[MAXPATH] = {};
         memcpy(buf, line.c_str(), line.size());
         struct work work;
-        parsetowork(d, buf, &work);
-
-        // since there should be a database file, open it
-        sqlite3 * db = nullptr;
-        if (sqlite3_open_v2(db_name.c_str(), &db, SQLITE_OPEN_READONLY, nullptr) != SQLITE_OK) {
-            std::cerr << "Could not open database file: " << db_name << std::endl;
-            return false;
-        }
+        linetowork(buf, csa->delim, &work);
 
         // extract the basename from the entry name
         char * bufbase = basename(work.name);
 
         // query the database for the current entry
-        char sql[1024];
-        snprintf(sql, 1024, "SELECT * FROM entries WHERE (name == '%s') AND (uid == %d) AND (gid == %d);", bufbase, work.statuso.st_uid, work.statuso.st_gid);
+        char sql[MAXSQL];
+        snprintf(sql, MAXSQL, "SELECT * FROM entries WHERE (name == '%s') AND (uid == %d) AND (gid == %d);", bufbase, work.statuso.st_uid, work.statuso.st_gid);
         int results = 0;
         char * err = nullptr;
         if (sqlite3_exec(db, sql, count_callback, &results, &err) != SQLITE_OK) {
             std::cerr << "SQLite error: " << err << ": " << sql << std::endl;
             sqlite3_free(err);
-            sqlite3_close(db);
-            return false;
+            rc = 1;
+            break;
         }
 
         if (results != 1) {
-            std::cerr << "Expected 1 row. Got " << results << ": " << parent << "/db.db " << bufbase << std::endl;
-            sqlite3_close(db);
-            return false;
+            std::cerr << "Did not find expected entry \"" << bufbase << "\" in " <<  db_name << std::endl;
+            rc = 1;
+            break;
         }
 
-        sqlite3_close(db);
+        entries++;
     }
 
-    return true;
+    sqlite3_close(db);
+
+    // previously counted entries should be the
+    // same as the number encountered here
+    if (sa->entries != entries) {
+        std::cerr << "Expected " << sa->entries << " entries. Got " << entries << "." << std::endl;
+        rc = 1;
+    }
+
+    delete sa;
+
+    if (rc != 0) {
+        csa->correct = false;
+    }
+
+    return rc;
 }
 
-void worker(std::mutex & correct_mutex, bool & correct,
-            std::atomic_bool & scouting,
-            char * trace_name,
-            const std::string & GUFI_tree,
-            ThreadWork & tw) {
-    std::ifstream trace(trace_name, std::ios::binary);
-    if (!trace) {
-        std::cerr << "Could not open " << trace_name << ". Stopping Thread" << std::endl;
-        correct = false;
-        return;
-    }
 
-    while (correct || scouting || tw.queue.size()) {
-        std::list <Row> dirs;
-        {
-            std::unique_lock <std::mutex> lock(tw.mutex);
-
-            // wait for work
-            while (correct && scouting && !tw.queue.size()) {
-                tw.cv.wait(lock);
-            }
-
-            if (!correct || (!scouting && !tw.queue.size())) {
-                break;
-            }
-
-            // // take single work item
-            // dirs.push_back(tw.queue.front());
-            // tw.queue.pop_front();
-
-            // take all work
-            dirs = std::move(tw.queue);
-            tw.queue.clear();
-        }
-
-        // process all work
-        for(Row & dir : dirs) {
-            // std::cout << tw.id << " " << dirs.size() << " " << dir->line << std::endl;
-            const bool rc = check_stanza(dir, trace, GUFI_tree);
-            std::lock_guard <std::mutex> lock(correct_mutex);
-            correct &= rc;
-        }
-    }
+// find first occurance of the delimiter
+std::string::size_type parsefirst(const char delim, struct StanzaStart * work) {
+    return (work->first_delim = work->line.find(delim));
 }
 
-int main(int argc, char * argv[]) {
-    if (argc < 3) {
-        std::cerr << "Syntax: " << argv[0] << " trace GUFI_tree [threads]" << std::endl;
-        return 0;
+int scout_function(struct QPTPool *ctx, const size_t id, void * data, void * args) {
+    struct CheckStanzaArgs * csa = static_cast <struct CheckStanzaArgs *> (args);
+    csa->correct = false;
+
+    char * filename = static_cast <char *> (data);
+
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "Could not open file " << filename << std::endl;
+        return 1;
     }
 
-    std::size_t thread_count = std::thread::hardware_concurrency();
-    if (argc > 3) {
-        std::stringstream s;
-        s << argv[3];
-
-        if (!(s >> thread_count)) {
-            std::cerr << "Error: Bad thread count: " << argv[3] << std::endl;
+    // open multiple copies of the trace file
+    // in preparation for check_stanza threads
+    for(std::size_t i = 0; i < csa->threads; i++) {
+        csa->traces.push_back(new std::ifstream(filename, std::ios::binary));
+        if (!*(csa->traces[i])) {
+            std::cerr << "Failed to open copies of " << filename << std::endl;
             return 1;
         }
     }
 
-    const std::string GUFI_tree = argv[2];
-
-    State state(thread_count);
-    std::mutex correct_mutex;
-    bool correct = true;
-    std::atomic_bool scouting(true);
-
-    // start threads
-    std::thread scout(scout_function, std::ref(correct), std::ref(scouting), argv[1], std::ref(state));
-    for(std::size_t i = 0; i < thread_count; i++) {
-        state[i].first.id = i;
-        state[i].second = std::thread(worker, std::ref(correct_mutex), std::ref(correct), std::ref(scouting), argv[1], std::ref(GUFI_tree), std::ref(state[i].first));
+    struct StanzaStart * curr = new struct StanzaStart();
+    if (!std::getline(file, curr->line)) {
+        delete curr;
+        return 1;
     }
 
-    // wait for all threads to finish
-    scout.join();
-    for(WorkPair & wp : state) {
-        wp.second.join();
+    if (parsefirst(csa->delim[0], curr) == std::string::npos) {
+        delete curr;
+        return 1;
     }
+
+    csa->correct = true;
+    curr->offset = file.tellg();
+
+    std::size_t file_count = 0;
+    std::size_t dir_count = 1; // always start with a directory
+    std::size_t empty = 0;
+
+    std::string line;
+    while (csa->correct && std::getline(file, line)) {
+        if (!line.size()) {
+            continue;
+        }
+
+        struct StanzaStart * next = new struct StanzaStart();
+        next->line = std::move(line);
+
+        if (parsefirst(csa->delim[0], next) == std::string::npos) {
+            delete curr;
+            delete next;
+            csa->correct = false;
+            break;
+        }
+
+        // push directories onto queues
+        if (next->line[next->first_delim + 1] == 'd') {
+            dir_count++;
+
+            empty += !curr->entries;
+            next->offset = file.tellg();
+
+            QPTPool_enqueue(ctx, id, check_stanza, curr);
+
+            curr = next;
+        }
+        else {
+            curr->entries++;
+            file_count++;
+            delete next;
+        }
+    }
+
+    // insert the last work item
+    if (csa->correct) {
+        QPTPool_enqueue(ctx, id, check_stanza, curr);
+    }
+
+    std::cerr << "Files: " << file_count << std::endl
+              << "Dirs:  " << dir_count << " (" << empty << " empty)" << std::endl
+              << "Total: " << file_count + dir_count << std::endl;
+
+    return 0;
+}
+
+int main(int argc, char * argv[]) {
+    if (argc < 4) {
+        std::cerr << "Syntax: " << argv[0] << " trace delim GUFI_tree [threads]" << std::endl;
+        return 0;
+    }
+
+    char * trace = argv[1];
+    char * delim = argv[2];
+    const std::string GUFI_tree = argv[3];
+
+    std::size_t threads = 1;
+    if (argc > 4) {
+        std::stringstream s;
+        s << argv[4];
+
+        if (!(s >> threads)) {
+            std::cerr << "Error: Bad thread count: " << argv[4] << std::endl;
+            return 1;
+        }
+    }
+
+    std::atomic_bool correct(false);
+
+    struct QPTPool * ctx = QPTPool_init(threads);
+
+    // start threads with partially initialized args
+    struct CheckStanzaArgs csa(threads, correct, delim, GUFI_tree);
+    QPTPool_start(ctx, &csa);
+
+    // start scouting function to push work onto queues
+    QPTPool_enqueue(ctx, 0, scout_function, trace);
+
+    QPTPool_wait(ctx);
+    QPTPool_destroy(ctx);
 
     std::cout << argv[1] << " " << (correct?"Pass":"Fail")  << std::endl;
 
