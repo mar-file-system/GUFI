db4e7f78b9c1369ca3cba23b9456aeec68d7c450
Jason Lee <jasonlee@lanl.gov>
2020-05-08 11:19:22 -0600

    SingleyLinkedList update
    Added sll_loop macro to simplify looping over a struct sll
    Added sll_move_append to move the contents of one sll into another, but appending to the destination instead of overwriting
        Added tests

--- include/SinglyLinkedList.h
+++ include/SinglyLinkedList.h
@@ -82,12 +82,19 @@ struct sll {
 struct sll * sll_init(struct sll * sll);
 struct sll * sll_push(struct sll * sll, void * data);
 struct sll * sll_move(struct sll * dst, struct sll * src);
+struct sll * sll_move_append(struct sll * dst, struct sll * src);
 size_t sll_get_size(struct sll * sll);
 
 /* functions for looping over a sll */
 struct node * sll_head_node(struct sll * sll);
 struct node * sll_next_node(struct node * node);
 void * sll_node_data(struct node * node);
+
+/* convenience macro */
+#define sll_loop(sll, name)                      \
+    for(struct node * name = sll_head_node(sll); \
+        name;                                    \
+        name = sll_next_node(name))              \
 
 void sll_destroy(struct sll * sll, void (*destroy)(void *));
 
