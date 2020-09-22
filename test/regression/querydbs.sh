73fe933435851b8aa2de6c5e8db309e5f76e0f59
Jason Lee <jasonlee@lanl.gov>
2020-09-22 15:43:49 -0600

    Renamed querydbn to querydbs

--- test/regression/querydbs.sh
+++ test/regression/querydbs.sh
@@ -1,5 +1,4 @@
-#!/bin/bash -e
-
+#!/usr/bin/env bash
 # This file is part of GUFI, which is part of MarFS, which is released
 # under the BSD license.
 #
@@ -62,15 +61,52 @@
 
 
 
-# arg 1 gufi tree arg 2 num threads
+set -e
 
-SCRIPT="$BASH_SOURCE" # Must be at the top
-DIR=`dirname $SCRIPT`
-BFQ=$DIR/../src/gufi_query
-QUERYDBN=$DIR/../src/querydbn
+ROOT="$(realpath ${BASH_SOURCE[0]})"
+ROOT="$(dirname ${ROOT})"
+ROOT="$(dirname ${ROOT})"
+ROOT="$(dirname ${ROOT})"
 
-echo "query to create db insert recs from entry query into out db big files and run querydbn"
-rm -f outdb*
-# if you want to be fancy then use date functions to provide this in user readable date/time
-$BFQ -E "insert into sument select uidtouser(uid, 5), path(''), name, size, mtime from entries where type='f'and size>4;" -n $2 -O outdb -I "create table sument (username text, pathname text, name text, size int64, mtime int64);" $1
-$QUERYDBN -NV outdb sument "select username, pathname, name, size, mtime from vsument order by size;" outdb.*
+BFQ="${ROOT}/src/gufi_query"
+QUERYDBS="${ROOT}/src/querydbs"
+OUTDB="outdb"
+TABLE="out"
+THREADS="2"
+
+# output directories
+SRCDIR="prefix"
+INDEXROOT="$(realpath ${SRCDIR}.gufi)"
+
+source ${ROOT}/test/regression/setup.sh "${ROOT}" "${SRCDIR}" "${INDEXROOT}"
+
+function cleanup() {
+    rm -f ${OUTDB} ${OUTDB}.*
+}
+
+trap cleanup EXIT
+
+cleanup
+
+OUTPUT="querydbs.out"
+
+function replace() {
+    echo "$@" | sed "s/${BFQ//\//\\/}/gufi_query/g; s/${QUERYDBS//\//\\/}/querydbs/g; s/${INDEXROOT//\//\\/}\\///g; s/${INDEXROOT//\//\\/}/./g; s/[[:space:]]*$//g"
+}
+
+(
+replace "# Use ${BFQ} to generate per-thread result database files"
+replace "${BFQ} -n ${THREADS} -O ${OUTDB} -I \"CREATE TABLE ${TABLE}(name TEXT, size INT64)\" -E \"INSERT INTO ${TABLE} SELECT path(summary.name) || '/' || pentries.name, pentries.size FROM summary, pentries WHERE (pentires.type=='f') AND (summary.inode == pentries.pinode)\" ${INDEXROOT}"
+output=$(${BFQ} -n ${THREADS} -O "${OUTDB}" -I "CREATE TABLE ${TABLE}(name TEXT, size INT64)" -E "INSERT INTO ${TABLE} SELECT path(summary.name) || '/' || pentries.name, pentries.size FROM summary, pentries WHERE (pentries.type=='f') AND (summary.inode == pentries.pinode)" "${INDEXROOT}")
+echo "${output}"
+
+replace "# Query all per-thread result databse files at once"
+replace  "${QUERYDBS} -NV outdb \"${TABLE}\" \"SELECT name, size FROM v${TABLE} ORDER BY size ASC, name ASC\" outdb.*"
+output=$(${QUERYDBS} -NV outdb "${TABLE}" "SELECT name, size FROM v${TABLE} ORDER BY size ASC, name ASC" outdb.*)
+replace "${output}"
+echo
+
+) | tee "${OUTPUT}"
+
+diff -b ${ROOT}/test/regression/querydbs.expected "${OUTPUT}"
+rm "${OUTPUT}"
