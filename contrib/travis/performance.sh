a9d436c120d07f9786fa8bae457ba356c0375deb
Jason Lee <jasonlee@lanl.gov>
2020-12-17 14:49:35 -0700

    specify full path

--- contrib/travis/performance.sh
+++ contrib/travis/performance.sh
@@ -79,9 +79,10 @@ gufi_dir2index -n "${CPUS}" "${ROOT}" "${INDEX}"
 cd "${ROOT}/contrib/performance"
 
 # run benchmarks on each executable
-for EXEC in gufi_query
+for EXEC in "${ROOT}/build/src/gufi_query"
 do
-    DB="${EXEC}.db"
+    name="$(basename ${EXEC})"
+    DB="${name}.db"
 
     # set up the database
     if [[ ! -f "${DB}" ]]
@@ -89,61 +90,62 @@ do
         ./initialize.py "${DB}" "${EXEC}"
     fi
 
-    CONFIG_HASH=$(./configuration.py --name "Travis ${TRAVIS_OS_NAME}"      \
-                                     --cpu "${TRAVIS_CPU_ARCH}"             \
-                                     --memory "7.5 GB"                      \
-                                     "${EXEC}"                              \
-                                     --threads "${CPUS}"                    \
-                                     --summary "SELECT * FROM summary"      \
-                                     --entries "SELECT * FROM entries"      \
-                                     --buffer_size 1024 --terse "${INDEX}"  \
+    CONFIG_HASH=$(./configuration.py --name "Travis ${TRAVIS_OS_NAME}"       \
+                                     --cpu "${TRAVIS_CPU_ARCH}"              \
+                                     --memory "7.5 GB"                       \
+                                     "${name}"                               \
+                                     --threads "${CPUS}"                     \
+                                     --summary "SELECT * FROM summary"       \
+                                     --entries "SELECT * FROM entries"       \
+                                     --buffer_size 1024 --terse "${INDEX}"   \
                                      --add "${DB}")
 
-    sudo ./run.py --add "${DB}" "${CONFIG_HASH}" --stat average "${EXEC}"   \
-        setup_globals=-1,1                                                  \
-        setup_aggregate=-1,1                                                \
-        work=-1,1                                                           \
-        opendir=-1,1                                                        \
-        opendb=-1,1                                                         \
-        sqlite3_open=-1,1                                                   \
-        set_pragmas=-1,1                                                    \
-        load_extension=-1,1                                                 \
-        addqueryfuncs=-1,1                                                  \
-        descend=-1,1                                                        \
-        check_args=-1,1                                                     \
-        level=-1,1                                                          \
-        level_branch=-1,1                                                   \
-        while_branch=-1,1                                                   \
-        readdir=-1,1                                                        \
-        readdir_branch=-1,1                                                 \
-        strncmp=-1,1                                                        \
-        strncmp_branch=-1,1                                                 \
-        snprintf=-1,1                                                       \
-        lstat=-1,1                                                          \
-        isdir=-1,1                                                          \
-        isdir_branch=-1,1                                                   \
-        access=-1,1                                                         \
-        set_struct=-1,1                                                     \
-        clone=-1,1                                                          \
-        pushdir=-1,1                                                        \
-        attach=-1,1                                                         \
-        sqltsumcheck=-1,1                                                   \
-        sqltsum=-1,1                                                        \
-        sqlsum=-1,1                                                         \
-        sqlent=-1,1                                                         \
-        detach=-1,1                                                         \
-        closedb=-1,1                                                        \
-        closedir=-1,1                                                       \
-        utime=-1,1                                                          \
-        free_work=-1,1                                                      \
-        output_timestamps=-1,1                                              \
-        aggregate=-1,1                                                      \
-        output=-1,1                                                         \
-        cleanup_globals=-1,1                                                \
-        rows=-1,1                                                           \
-        query_count=-1,1                                                    \
-        RealTime=-1,1                                                       \
-        ThreadTime=-1,1
+    sudo ./run.py --add "${DB}" "${CONFIG_HASH}" --executable-path="${EXEC}" \
+         --stat average "${name}"                                            \
+         setup_globals=-1,1                                                  \
+         setup_aggregate=-1,1                                                \
+         work=-1,1                                                           \
+         opendir=-1,1                                                        \
+         opendb=-1,1                                                         \
+         sqlite3_open=-1,1                                                   \
+         set_pragmas=-1,1                                                    \
+         load_extension=-1,1                                                 \
+         addqueryfuncs=-1,1                                                  \
+         descend=-1,1                                                        \
+         check_args=-1,1                                                     \
+         level=-1,1                                                          \
+         level_branch=-1,1                                                   \
+         while_branch=-1,1                                                   \
+         readdir=-1,1                                                        \
+         readdir_branch=-1,1                                                 \
+         strncmp=-1,1                                                        \
+         strncmp_branch=-1,1                                                 \
+         snprintf=-1,1                                                       \
+         lstat=-1,1                                                          \
+         isdir=-1,1                                                          \
+         isdir_branch=-1,1                                                   \
+         access=-1,1                                                         \
+         set_struct=-1,1                                                     \
+         clone=-1,1                                                          \
+         pushdir=-1,1                                                        \
+         attach=-1,1                                                         \
+         sqltsumcheck=-1,1                                                   \
+         sqltsum=-1,1                                                        \
+         sqlsum=-1,1                                                         \
+         sqlent=-1,1                                                         \
+         detach=-1,1                                                         \
+         closedb=-1,1                                                        \
+         closedir=-1,1                                                       \
+         utime=-1,1                                                          \
+         free_work=-1,1                                                      \
+         output_timestamps=-1,1                                              \
+         aggregate=-1,1                                                      \
+         output=-1,1                                                         \
+         cleanup_globals=-1,1                                                \
+         rows=-1,1                                                           \
+         query_count=-1,1                                                    \
+         RealTime=-1,1                                                       \
+         ThreadTime=-1,1
 
     ./dump.py "${DB}" "${EXEC}" "${CONFIG_HASH}" "${STAT}" > "${EXEC}.${STAT}"
 
