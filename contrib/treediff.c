d30898f8e441e2b9ba71ee472472fab797f0d779
Jason Lee <jasonlee@lanl.gov>
2023-12-21 16:03:23 -0700

    simple treediff
    prints top-most directory that is different and stops
    only handles name mismatches

---
+++ contrib/treediff.c
@@ -0,0 +1,400 @@
+/*
+This file is part of GUFI, which is part of MarFS, which is released
+under the BSD license.
+
+
+Copyright (c) 2017, Los Alamos National Security (LANS), LLC
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+From Los Alamos National Security, LLC:
+LA-CC-15-039
+
+Copyright (c) 2017, Los Alamos National Security, LLC All rights reserved.
+Copyright 2017. Los Alamos National Security, LLC. This software was produced
+under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National
+Laboratory (LANL), which is operated by Los Alamos National Security, LLC for
+the U.S. Department of Energy. The U.S. Government has rights to use,
+reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR LOS
+ALAMOS NATIONAL SECURITY, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is
+modified to produce derivative works, such modified software should be
+clearly marked, so as not to confuse it with the version available from
+LANL.
+
+THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL LOS ALAMOS NATIONAL SECURITY, LLC OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+
+
+#include <dirent.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "QueuePerThreadPool.h"
+#include "bf.h"
+#include "print.h"
+#include "trie.h"
+#include "utils.h"
+
+struct PoolArgs {
+    struct input in;
+    trie_t *skip;
+    struct OutputBuffers obufs;
+};
+
+/*
+ * treated as POD and simply copy instead of
+ * doing 2 dynamic allocations per instance
+ */
+typedef struct str {
+    char *data;
+    size_t len;
+} str_t;
+
+static inline str_t str_copy_construct(const char *str, const size_t len) {
+    str_t dst;
+    dst.len = len;
+    dst.data = malloc(len + 1);
+    SNFORMAT_S(dst.data, dst.len + 1, 1, str, len);
+    return dst;
+}
+
+static inline void str_destruct(str_t str) {
+    free(str.data);
+}
+
+static int str_cmp(const void *l, const void *r) {
+    str_t *lhs = (str_t *) l;
+    str_t *rhs = (str_t *) r;
+
+    const size_t len = (lhs->len < rhs->len)?lhs->len:rhs->len;
+    return strncmp(lhs->data, rhs->data, len + 1);
+}
+
+static int get_entries(DIR *dir, trie_t *skip_db,
+                       str_t *subdirs, const nlink_t spaces) {
+    nlink_t found = 0;
+
+    struct dirent *entry = NULL;
+    while (((entry = readdir(dir))) && (found < spaces)) {
+        const size_t len = strlen(entry->d_name);
+
+        /* skip . and .. and *.db */
+        const int skip = (trie_search(skip_db, entry->d_name, len) ||
+                          ((len >= 3) && (strncmp(entry->d_name + len - 3, ".db", 3) == 0)));
+        if (skip) {
+            continue;
+        }
+
+        /* only pull directories */
+        if (entry->d_type != DT_DIR) {
+            continue;
+        }
+
+        subdirs[found++] = str_copy_construct(entry->d_name, len);
+    }
+
+    /* should return found, but found does not say whther there are unprocessed entries */
+    return !!entry;
+}
+
+/*
+ * Compare Paths
+ * Currently, the only directory names are compared.
+ *
+ * Possible expansions:
+ *     Types
+ *     Sizes
+ *     UID/GID
+ *     Permissions
+ *     Timestamps
+ *     db.db contents
+ *         Rollups
+ *     Handle/Detect directory moves
+ */
+static int compare_paths(str_t lhs, str_t rhs) {
+    return str_cmp(&lhs, &rhs);
+}
+
+struct ComparePaths {
+    size_t level;
+    str_t lhs;
+    str_t rhs;
+};
+
+#define ERROR_AND_JUMP(rc, err, label, fmt, ...)    \
+    do {                                            \
+        const int err = errno;                      \
+        (void) err;                                 \
+        if (strlen(fmt)) {                          \
+            fprintf(stderr, fmt, __VA_ARGS__);      \
+        }                                           \
+        (rc) = 1;                                   \
+        goto label;                                 \
+    } while (0)
+
+static int processdir(QPTPool_t *ctx, const size_t id, void *data, void *args) {
+    struct ComparePaths *cp = (struct ComparePaths *) data;
+    struct PoolArgs *pa = (struct PoolArgs *) args;
+
+    int rc = 0;
+
+    if (cp->level >= pa->in.max_level) {
+        goto cleanup;
+    }
+
+    /* ********************************************** */
+    struct stat lst;
+    struct stat rst;
+
+    if (lstat(cp->lhs.data, &lst) != 0) {
+        ERROR_AND_JUMP(rc, err, cleanup,
+                       "Error: Could not stat left directory \"%s\": %s (%d)\n",
+                       cp->lhs.data, strerror(err), err);
+    }
+
+    if (lstat(cp->rhs.data, &rst) != 0) {
+        ERROR_AND_JUMP(rc, err, cleanup,
+                       "Error: Could not stat right directory \"%s\": %s (%d)\n",
+                       cp->rhs.data, strerror(err), err);
+    }
+    /* ********************************************** */
+
+    /* ********************************************** */
+    DIR *ldir = NULL;
+    DIR *rdir = NULL;
+
+    if (!(ldir = opendir(cp->lhs.data))) {
+        ERROR_AND_JUMP(rc, err, closedir,
+                       "Error: Could not open left directory \"%s\": %s (%d)\n",
+                       cp->lhs.data, strerror(err), err);
+    }
+
+    if (!(rdir = opendir(cp->rhs.data))) {
+        ERROR_AND_JUMP(rc, err, closedir,
+                       "Error: Could not open right directory \"%s\": %s (%d)\n",
+                       cp->rhs.data, strerror(err), err);
+    }
+    /* ********************************************** */
+
+    /* hard link count without . and .. */
+    const nlink_t lcount = lst.st_nlink - 2;
+    const nlink_t rcount = rst.st_nlink - 2;
+
+    /* ********************************************** */
+    /* get list of subdirs in each directory */
+    str_t *lsubdir = calloc(lcount, sizeof(*lsubdir));
+    str_t *rsubdir = calloc(rcount, sizeof(*rsubdir));
+    get_entries(ldir, pa->skip, lsubdir, lcount);
+    get_entries(rdir, pa->skip, rsubdir, rcount);
+    /* ********************************************** */
+
+    /* ********************************************** */
+    /* sort the paths to be able to find matches in linear time */
+    qsort(lsubdir, lcount, sizeof(*lsubdir), str_cmp);
+    qsort(rsubdir, rcount, sizeof(*rsubdir), str_cmp);
+    /* ********************************************** */
+
+    /* ********************************************** */
+    const size_t next_level = cp->level + 1;
+
+    struct PrintArgs print;
+    print.output_buffer = &pa->obufs.buffers[id];
+    print.delim = '/';
+    print.mutex = pa->obufs.mutex;
+    print.outfile = stdout;
+    print.rows = 0;
+
+    char *buf[] = {NULL, NULL};     /* passed to print_parallel */
+
+    /* mergesort */
+    nlink_t lidx = 0;
+    nlink_t ridx = 0;
+    while ((lidx < lcount) && (ridx < rcount)) {
+        const int diff = compare_paths(lsubdir[lidx], rsubdir[ridx]);
+
+        /*
+         * if diff != 0, print the path and stop
+         *
+         * subdirectories are not traversed in order to not have to
+         * process/print potentially massive subtrees when just
+         * printing the highest path already implies that everything
+         * underneath is different
+         */
+
+        if (diff < 0) {
+            buf[0] = cp->lhs.data;
+            buf[1] = lsubdir[lidx].data;
+            print_parallel(&print, 2, buf, NULL);
+            str_destruct(lsubdir[lidx]);
+            lidx++;
+        }
+        else if (diff == 0) {
+            struct ComparePaths *match = malloc(sizeof(*match));
+            match->level = next_level;
+
+            match->lhs.len = cp->lhs.len + 1 + lsubdir[lidx].len;
+            match->lhs.data = malloc(match->lhs.len + 1);
+            SNFORMAT_S(match->lhs.data, match->lhs.len + 1, 3,
+                       cp->lhs.data, cp->lhs.len,
+                       "/", (size_t) 1,
+                       lsubdir[lidx].data, lsubdir[lidx].len);
+
+            match->rhs.len = cp->rhs.len + 1 + rsubdir[ridx].len;
+            match->rhs.data = malloc(match->rhs.len + 1);
+            SNFORMAT_S(match->rhs.data, match->rhs.len + 1, 3,
+                       cp->rhs.data, cp->rhs.len,
+                       "/", (size_t) 1,
+                       rsubdir[ridx].data, rsubdir[ridx].len);
+
+            QPTPool_enqueue(ctx, id, processdir, match);
+
+            str_destruct(rsubdir[ridx]);
+            str_destruct(lsubdir[lidx]);
+
+            lidx++;
+            ridx++;
+        }
+        else if (diff > 0) {
+            buf[0] = cp->rhs.data;
+            buf[1] = rsubdir[ridx].data;
+            print_parallel(&print, 2, buf, NULL);
+            str_destruct(rsubdir[ridx]);
+            ridx++;
+        }
+    }
+
+    for(nlink_t i = lidx; i < lcount; i++) {
+        buf[0] = cp->lhs.data;
+        buf[1] = lsubdir[i].data;
+        print_parallel(&print, 2, buf, NULL);
+        str_destruct(lsubdir[i]);
+    }
+
+    for(nlink_t i = ridx; i < rcount; i++) {
+        buf[0] = cp->rhs.data;
+        buf[1] = rsubdir[i].data;
+        print_parallel(&print, 2, buf, NULL);
+        str_destruct(rsubdir[i]);
+    }
+    /* ********************************************** */
+
+    free(rsubdir);
+    free(lsubdir);
+
+  closedir:
+    closedir(rdir);
+    closedir(ldir);
+
+  cleanup:
+    str_destruct(cp->rhs);
+    str_destruct(cp->lhs);
+    free(cp);
+
+    return rc;
+}
+
+static void sub_help(void) {
+    printf("lhs               starting path of tree used on left hand side of this comparision\n");
+    printf("rhs               starting path of tree used on right hand side of this comparision\n");
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    struct PoolArgs pa;
+    int idx = parse_cmd_line(argc, argv, "hHn:z:", 2, "lhs rhs", &pa.in);
+    if (pa.in.helped)
+        sub_help();
+    if (idx < 0)
+        return 1;
+    else {
+        INSTALL_STR(&pa.in.name,   argv[argc - 2]);
+        INSTALL_STR(&pa.in.nameto, argv[argc - 1]);
+    }
+
+    int rc = 0;
+
+    /* skip . and .. */
+    if (setup_directory_skip(pa.in.skip.data, &pa.skip) != 0) {
+        rc = 1;
+        goto done;
+    }
+
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    if (!OutputBuffers_init(&pa.obufs, pa.in.maxthreads, pa.in.output_buffer_size, &mutex)) {
+        rc = 1;
+        goto cleanup_trie;
+    }
+
+    const uint64_t queue_depth = pa.in.target_memory_footprint / sizeof(struct ComparePaths) / pa.in.maxthreads;
+    QPTPool_t *pool = QPTPool_init_with_props(pa.in.maxthreads, &pa, NULL, NULL, queue_depth, 1, 2
+                                              #if defined(DEBUG) && defined(PER_THREAD_STATS)
+                                              , NULL
+                                              #endif
+        );
+    if (QPTPool_start(pool) != 0) {
+        fprintf(stderr, "Error: Failed to start thread pool\n");
+        rc = 1;
+        goto cleanup_qptp;
+    }
+
+    struct ComparePaths *cp = malloc(sizeof(*cp));
+    cp->level = 0;
+    cp->lhs   = str_copy_construct(pa.in.name.data,   pa.in.name.len);
+    cp->rhs   = str_copy_construct(pa.in.nameto.data, pa.in.nameto.len);
+
+    QPTPool_enqueue(pool, 0, processdir, cp);
+
+  cleanup_qptp:
+    QPTPool_wait(pool);
+    QPTPool_destroy(pool);
+
+    OutputBuffers_flush_to_single(&pa.obufs, stdout);
+    OutputBuffers_destroy(&pa.obufs);
+
+  cleanup_trie:
+    trie_free(pa.skip);
+
+  done:
+    return rc;
+}
