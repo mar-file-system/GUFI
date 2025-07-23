dc8c162843284a2b0c8e9b311a51a4c6a3b6d639
Thomas Bertschinger <bertschinger@lanl.gov>
2025-07-23 15:09:47 -0400

    gufi_dir2index: remove unnecessary manipulation of entry->name
    `entry->name` has the full path, but was being overwritten with just the
    relative path. This isn't actually necessary for any GUFI functionality.
    
    Previously it was harmless, but it will be problematic for handing the
    file to a plugin to process, since the plugin will need the full path.
    
    So in preparation for hooking in the plugin, get rid of the modification
    to entry->name.

--- src/gufi_dir2index.c
+++ src/gufi_dir2index.c
@@ -149,13 +149,6 @@ static int process_nondir(struct work *entry, struct entry_data *ed, void *args)
         external_read_file(in, entry, process_external, nda->db);
     }
 
-    /* get entry relative path (use extra buffer to prevent memcpy overlap) */
-    char relpath[MAXPATH];
-    const size_t relpath_len = SNFORMAT_S(relpath, MAXPATH, 1,
-                                          entry->name + entry->root_parent.len, entry->name_len - entry->root_parent.len);
-    /* overwrite full path with relative path */
-    entry->name_len = SNFORMAT_S(entry->name, MAXPATH, 1, relpath, relpath_len);
-
     /* update summary table */
     sumit(&nda->summary, entry, ed);
 
