mar-file-system/GUFI

commit a2cb2d7a85180479641afc142219f1d341d30a31

add generic data member to PluginCommonStruct

src/gufi_dir2index.c
@@ -111,8 +111,7 @@ struct NonDirArgs {
     struct entry_data ed;
 
     /* index path */
-    char *topath;
-    size_t topath_len;
+    str_t topath;
 
     /* summary of the current directory */
     struct sum summary;
@@ -155,7 +154,7 @@ static int process_nondir(struct work *entry, struct entry_data *ed, void *args)
     if (in->process_xattrs) {
         insertdbgo_xattrs(in, &nda->work->statuso, entry, ed,
                           &nda->xattr_db_list, nda->temp_xattr,
-                          nda->topath, nda->topath_len,
+                          nda->topath.data, nda->topath.len,
                           nda->xattrs_res, nda->xattr_files_res);
     }
 
@@ -197,11 +196,11 @@ static int processdir(QPTPool_ctx_t *ctx, void *data) {
     nda.temp_xattr = &pa->xattr;
     nda.work       = NULL;
     memset(&nda.ed, 0, sizeof(nda.ed));
-    nda.topath     = NULL;
     nda.ed.type    = 'd';
     nda.plugin_user_data = NULL;
 
     PCS_t pcs; /* references passed into plugin */
+    memset(&pcs, 0, sizeof(pcs));
 
     DIR *dir = NULL;
 
@@ -224,27 +223,27 @@ static int processdir(QPTPool_ctx_t *ctx, void *data) {
     }
 
     /* offset by work->root_len to remove prefix */
-    nda.topath_len = pa->index_parent.len + 1 + nda.work->name_len - nda.work->root_parent.len;
+    nda.topath.len = pa->index_parent.len + 1 + nda.work->name_len - nda.work->root_parent.len;
 
     /*
      * allocate space for "/db.db" in nda.topath
      *
      * extra buffer is not needed and save on memcpy-ing
      */
-    const size_t topath_size = nda.topath_len + 1 + DBNAME_LEN + 1;
+    const size_t topath_size = nda.topath.len + 1 + DBNAME_LEN + 1;
 
-    nda.topath = malloc(topath_size);
-    SNFORMAT_S(nda.topath, topath_size, 4,
+    nda.topath.data = malloc(topath_size);
+    SNFORMAT_S(nda.topath.data, topath_size, 4,
                pa->index_parent.data, pa->index_parent.len,
                "/", (size_t) 1,
                nda.work->name + nda.work->root_parent.len, nda.work->name_len - nda.work->root_parent.len,
                "\0" DBNAME, (size_t) 1 + DBNAME_LEN);
 
     /* don't need recursion because parent is guaranteed to exist */
-    if (mkdir(nda.topath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) < 0) {
+    if (mkdir(nda.topath.data, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) < 0) {
         const int err = errno;
         if (err != EEXIST) {
-            fprintf(stderr, "mkdir %s failure: %d %s\n", nda.topath, err, strerror(err));
+            fprintf(stderr, "mkdir %s failure: %d %s\n", nda.topath.data, err, strerror(err));
             rc = 1;
             goto close_dir;
         }
@@ -256,12 +255,12 @@ static int processdir(QPTPool_ctx_t *ctx, void *data) {
      */
     if (process_dir) {
         /* restore "/db.db" */
-        nda.topath[nda.topath_len] = '/';
+        nda.topath.data[nda.topath.len] = '/';
 
-        nda.db = template_to_db(nda.temp_db, nda.topath, nda.work->statuso.st_uid, nda.work->statuso.st_gid);
+        nda.db = template_to_db(nda.temp_db, nda.topath.data, nda.work->statuso.st_uid, nda.work->statuso.st_gid);
 
         /* remove "/db.db" */
-        nda.topath[nda.topath_len] = '\0';
+        nda.topath.data[nda.topath.len] = '\0';
 
         if (!nda.db) {
             rc = 1;
@@ -271,6 +270,7 @@ static int processdir(QPTPool_ctx_t *ctx, void *data) {
         pcs.db = nda.db;
         pcs.work = nda.work;
         pcs.ed = &nda.ed;
+        pcs.data = &nda.topath;
 
         /* prepare to insert into the database */
         zeroit(&nda.summary);
@@ -349,8 +349,8 @@ static int processdir(QPTPool_ctx_t *ctx, void *data) {
     }
 
     /* ignore errors */
-    chmod(nda.topath, nda.work->statuso.st_mode);
-    chown(nda.topath, nda.work->statuso.st_uid, nda.work->statuso.st_gid);
+    chmod(nda.topath.data, nda.work->statuso.st_mode);
+    chown(nda.topath.data, nda.work->statuso.st_uid, nda.work->statuso.st_gid);
 
   close_dir:
     closedir(dir);
@@ -362,7 +362,7 @@ static int processdir(QPTPool_ctx_t *ctx, void *data) {
         pa->total_nondirs[id] += ctrs.nondirs_processed;
     }
 
-    free(nda.topath);
+    free(nda.topath.data);
     free(nda.work);
 
     return rc;

include/plugin.h
@@ -130,6 +130,7 @@ typedef struct PluginCommonStruct {
     sqlite3 *db;
     struct work *work;
     struct entry_data *ed;
+    void *data; /* any extra data to pass along */
 } PCS_t;
 
 #ifdef __cplusplus

commit 36020c82cdfa5e6ce1850d05ef8b2d8fd55ac157

gufi_dir2index: nda.topath used unitialized in error case
Trevor reported that gufi_dir2index was seg faulting when running in
production. We ran it under ASAN and saw the following double-free was
reported:

```
$ ./gufi_dir2index --threads 64 /users /output
"/output" Already exists!
Creating GUFI tree /output with 64 threads
Error: Could not open directory "/users/some/long/path/": No such file or directory (2)
=================================================================
==3028882==ERROR: AddressSanitizer: attempting double-free on 0x7c922f276880 in thread T26:
    #0 0x0000004d0d3a in free (/opt/hpcinf/gufi-builds/GUFI-d3f09192/build-debug/src/gufi_dir2index+0x4d0d3a) (BuildId: 2bd9fc7a06341e3e8ed7d07a556d0735c9b9ea01)
    #1 0x0000005172cb in processdir /opt/hpcinf/gufi-builds/GUFI-d3f09192/src/gufi_dir2index.c:365:5
    #2 0x00000051c83d in process_work /opt/hpcinf/gufi-builds/GUFI-d3f09192/src/QueuePerThreadPool.c:313:36
    #3 0x000000519e63 in worker_function /opt/hpcinf/gufi-builds/GUFI-d3f09192/src/QueuePerThreadPool.c:356:35
    #4 0x0000004cea3c in asan_thread_start(void*) asan_interceptors.cpp.o
    #5 0x7fd23015b1c9 in start_thread (/lib64/libpthread.so.0+0x81c9) (BuildId: 601e076f2ed363fb96f707b98d1144cdd94878b8)
    #6 0x7fd22f77e952 in clone (/lib64/libc.so.6+0x39952) (BuildId: 1faac7cdefc71ce73027e33a84650684eecd1635)

0x7c922f276880 is located 0 bytes inside of 120-byte region [0x7c922f276880,0x7c922f2768f8)
freed by thread T28 here:
    #0 0x0000004d0d3a in free (/opt/hpcinf/gufi-builds/GUFI-d3f09192/build-debug/src/gufi_dir2index+0x4d0d3a) (BuildId: 2bd9fc7a06341e3e8ed7d07a556d0735c9b9ea01)
    #1 0x000000599a94 in clearSelect sqlite3.c

previously allocated by thread T28 here:
    #0 0x0000004d0fd8 in malloc (/opt/hpcinf/gufi-builds/GUFI-d3f09192/build-debug/src/gufi_dir2index+0x4d0fd8) (BuildId: 2bd9fc7a06341e3e8ed7d07a556d0735c9b9ea01)
    #1 0x000000577433 in sqlite3MemMalloc sqlite3.c

Thread T26 created by T0 here:
    #0 0x0000004b5595 in pthread_create (/opt/hpcinf/gufi-builds/GUFI-d3f09192/build-debug/src/gufi_dir2index+0x4b5595) (BuildId: 2bd9fc7a06341e3e8ed7d07a556d0735c9b9ea01)
    #1 0x000000519b96 in QPTPool_start /opt/hpcinf/gufi-builds/GUFI-d3f09192/src/QueuePerThreadPool.c:698:21
    #2 0x000000513da6 in main /opt/hpcinf/gufi-builds/GUFI-d3f09192/src/gufi_dir2index.c:593:9
    #3 0x7fd22f77f864 in __libc_start_main (/lib64/libc.so.6+0x3a864) (BuildId: 1faac7cdefc71ce73027e33a84650684eecd1635)

Thread T28 created by T0 here:
    #0 0x0000004b5595 in pthread_create (/opt/hpcinf/gufi-builds/GUFI-d3f09192/build-debug/src/gufi_dir2index+0x4b5595) (BuildId: 2bd9fc7a06341e3e8ed7d07a556d0735c9b9ea01)
    #1 0x000000519b96 in QPTPool_start /opt/hpcinf/gufi-builds/GUFI-d3f09192/src/QueuePerThreadPool.c:698:21
    #2 0x000000513da6 in main /opt/hpcinf/gufi-builds/GUFI-d3f09192/src/gufi_dir2index.c:593:9
    #3 0x7fd22f77f864 in __libc_start_main (/lib64/libc.so.6+0x3a864) (BuildId: 1faac7cdefc71ce73027e33a84650684eecd1635)

SUMMARY: AddressSanitizer: double-free (/opt/hpcinf/gufi-builds/GUFI-d3f09192/build-debug/src/gufi_dir2index+0x4d0d3a) (BuildId: 2bd9fc7a06341e3e8ed7d07a556d0735c9b9ea01) in free
==3028882==ABORTING
```

This is kind of confusing because it's not clear why nda.topath.data,
freed at the end of processdir() (gufi_dir2index.c:365 at commit
d3f09192), would have been originally allocated
by sqlite3MemMalloc().

However, the error message about the directory not existing gives a
clue.

It looks like when that error is hit at the beginning of processdir(),
control jumps to the cleanup label before nda.topath is initialized.
Thus, free(nda.topath.data) is given a garbage value to free. It looks
like by chance that ended up being a previously freed sqlite object.

The solution is to initialize nda.topath at the beginning of
processdir().

Reported-by: Trevor Bautista <tbautista@lanl.gov>
Fixes: a2cb2d7a8518

src/gufi_dir2index.c
@@ -198,6 +198,11 @@ static int processdir(QPTPool_ctx_t *ctx, void *data) {
     memset(&nda.ed, 0, sizeof(nda.ed));
     nda.ed.type    = 'd';
     nda.plugin_user_data = NULL;
+    nda.topath = (str_t) {
+        .data = NULL,
+        .len = 0,
+        .free = NULL,
+    };
 
     PCS_t pcs; /* references passed into plugin */
     memset(&pcs, 0, sizeof(pcs));