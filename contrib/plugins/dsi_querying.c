095029e2d299a25f6b7034c063bc62b1c949eeaf
Jason Lee <jasonlee@lanl.gov>
2026-05-14 20:38:08 +0000

    DSI plugin usage change
    indexing
        No longer creating symlinks because the individual directories do
        not need to know where the collection database is during querying
    
        Collection database is no longer renamed to <collection name>.db
    
    querying
        Old usage:
            - CREATE VIRTUAL TABLE results USING gufi_vt(
                E = "SELECT ..., dsi_uuid(xattr_name, xattr_value) AS uuid
                     FROM vrxpentries
                     WHERE (dsi_collection_name(xattr_name) == '<collection name>') AND
                           (uuid == <collection name>.<table>.uuid);
              );
    
            This used hidden automatic ATTACHes and ATTACH-ed ALL
            discovered collection databases. Unused collection databases
            result in wasted opens. Additionally, massive amounts of
            processing is potentially wasted due to low match rate at each
            directory (e.g. few files per directory with many UUIDS in the
            collection database to compare with).
    
        New usage:
            - CREATE VIRTUAL TABLE results USING gufi_vt(
                E = "SELECT ..., dsi_uuid(xattr_name, xattr_value)
                     FROM vrxpentries
                     WHERE dsi_collection_name(xattr_name) == '<collection name>';
              );
    
            - ATTACH '<locally/accessible/collection/db/path>' AS <collection attach name>;
    
            - SELECT ...
              FROM results
              JOIN <collection attach name>
              ON results.uuid == <collection attach name>.<table>.uuid;
    
            OR
    
            - CREATE VIRTUAL TABLE results USING gufi_vt(
                I = "CREATE TABLE intermediate(..., uuid TEXT);",
    
                E = "INSERT INTO intermediate
                     SELECT ..., dsi_uuid(xattr_name, xattr_value)
                     FROM vrxpentries
                     WHERE dsi_collection_name(xattr_name) == '<collection name>';",
    
                K = "CREATE TABLE aggregate(..., uuid TEXT);
                     ATTACH '<path/to/collection/db/copy/in/index>' AS <collection attach name>;",
    
                J = "INSERT INTO aggregate SELECT ..., uuid FROM intermediate;",
    
                G = "SELECT ...
                     FROM aggregate
                     JOIN <collection attach name>
                     ON aggregate.uuid == <collection attach name>.<table>.uuid;"
              );
    
            - SELECT * FROM results;
    
            These do one JOIN instead of a JOIN at each directory.
    
            These also allow for the collection database to be located
            anywhere because it is no longer used in the plugin context for
            ATTACH-ing (the path is kept in the xattr to allow for the
            original collection database to be found if necessary). However,
            because indexing still requires a good location to copy the
            collection database to, the previous rules still apply (needs be
            in the tree being indexed, at or above the first time it is used).

--- contrib/plugins/dsi_querying.c
+++ contrib/plugins/dsi_querying.c
@@ -68,10 +68,9 @@ OF SUCH DAMAGE.
 
 #include <sqlite3.h>
 
-#include "SinglyLinkedList.h"
+#include "bf.h"
 #include "dsi.h"
 #include "plugin.h"
-#include "utils.h"           /* SNPRINTF, SNFORMAT_S */
 
 /*
  * dsi_collection_name(xattr_name)
@@ -132,12 +131,6 @@ static void dsi_dbpath(sqlite3_context *context, int argc, sqlite3_value **argv)
     sqlite3_result_text(context, value + DSI_VALUE_PREFIX_LEN, -1, SQLITE_TRANSIENT);
 }
 
-/* used for both plugin_user_data and sqlite3_exec callback */
-struct DSI_Querying_State {
-    sqlite3 *db;
-    sll_t attached; /* list of collection name strings */
-};
-
 static int dsi_querying_global_init(void *global) {
     struct input *in = (struct input *) global;
     in->process_xattrs = 1; /* automatically enable xattr processing if not already */
@@ -145,76 +138,6 @@ static int dsi_querying_global_init(void *global) {
     sqlite3_initialize();
     return 0;
 }
-
-/* attach collection dbs once using the directory's xattrs instead of once per file/link */
-const char ATTACH_COLLECTIONDB_SQL[] = "SELECT rpath(sname, sroll), xattr_name, xattr_value FROM vrxsummary "
-                                       "WHERE xattr_name IS NOT NULL;";
-static int attach_collectiondb_cb(void *args, int count, char **data, char **columns) {
-    struct DSI_Querying_State *dsiqs = (struct DSI_Querying_State *) args;
-
-    (void) count;
-    (void) columns;
-
-    const char *pwd        = data[0];
-    const char *xattr_name = data[1];
-
-    /* xattr_name == NULL has already been checked */
-    const size_t xattr_name_len = strlen(xattr_name);
-
-    /* not a DSI collection db name */
-    if (!is_dsi_xattr(xattr_name, xattr_name_len)) {
-        return 0;
-    }
-
-    const size_t collection_name_len = xattr_name_len - DSI_NAME_PREFIX_LEN;
-    const char *collection_name = xattr_name + DSI_NAME_PREFIX_LEN;
-
-    /* check if attaching will work - correct check requires SQL - just do simple check here */
-    if (sll_get_size(&dsiqs->attached) == 253) { /* max 254 - 1 (db.db) */
-        fprintf(stderr, "Warning: Not attaching %s: Too many attaches\n", collection_name);
-        return 0;
-    }
-
-    /* get the copied collection db file path */
-    const size_t pwd_len = strlen(pwd);
-    const size_t collection_dbname_len = pwd_len + 1 + collection_name_len + 3;
-    char *collection_dbname = malloc(collection_dbname_len + 1);
-    SNFORMAT_S(collection_dbname, collection_dbname_len + 1, 4,
-               pwd, pwd_len,
-               "/", (size_t) 1,
-               collection_name, collection_name_len,
-               ".db", (size_t) 3);
-
-    /* copy the collection name */
-    char *attach_name = strndup(collection_name, collection_name_len);
-
-    /* attach collection db as the collection name */
-    char attach_sql[MAXSQL];
-    SNPRINTF(attach_sql, sizeof(attach_sql), "ATTACH 'file:%s?mode=ro' AS '%s';",
-             collection_dbname, attach_name);
-
-    if (sqlite3_exec(dsiqs->db, attach_sql, NULL, NULL, NULL) != SQLITE_OK) {
-        /*
-         * collection db not existing or not readable is an error even
-         * if the specific collection is not being used at the moment
-         */
-        const char *xattr_value = data[2];
-        const char *orig_db     = xattr_value + DSI_VALUE_PREFIX_LEN;
-        fprintf(stderr, "Error: Could not attach local copy of collection db for collection \"%s\" (original file: \"%s\") at \"%s\"\n",
-                collection_name, orig_db, pwd); /* not printing actual error message */
-        free(attach_name);
-        free(collection_dbname);
-        return 1;
-    }
-
-    /* keep attach name for detaching */
-    sll_push_back(&dsiqs->attached, attach_name);
-
-    free(collection_dbname);
-    return 0;
-}
-
-static void dsi_querying_ctx_exit(void *ptr, void *plugin_user_data);
 
 static void *dsi_querying_ctx_init(void *ptr) {
     sqlite3 *db = (sqlite3 *) ptr;
@@ -226,50 +149,9 @@ static void *dsi_querying_ctx_init(void *ptr) {
         (sqlite3_create_function(db,   "dsi_dbpath",          2, SQLITE_UTF8,
                                  NULL, &dsi_dbpath,           NULL, NULL) != SQLITE_OK)) {
         fprintf(stderr, "Error: Could not create DSI functions\n");
-        return NULL;
     }
 
-    struct DSI_Querying_State *dsiqs = malloc(sizeof(*dsiqs));
-    dsiqs->db = db;
-    sll_init(&dsiqs->attached);
-
-    /* attach all collection dbs known to this file */
-    char *err = NULL;
-    if (sqlite3_exec(db, ATTACH_COLLECTIONDB_SQL, attach_collectiondb_cb, dsiqs, &err) != SQLITE_OK) {
-        fprintf(stderr, "Error: Could not get xattrs for attaching collection dbs: %s\n", err);
-        sqlite3_free(err);
-        dsi_querying_ctx_exit(ptr, dsiqs);
-        return NULL;
-    }
-
-    return dsiqs;
-}
-
-static void dsi_querying_ctx_exit(void *ptr, void *plugin_user_data) {
-    (void) ptr;
-
-    struct DSI_Querying_State *dsiqs = (struct DSI_Querying_State *) plugin_user_data;
-    if (!dsiqs) {
-        return;
-    }
-
-    /* detach all collection dbs */
-    sll_loop(&dsiqs->attached, node) {
-        const char *attach_name = sll_node_data(node);
-
-        char detach_sql[MAXSQL];
-        SNPRINTF(detach_sql, sizeof(detach_sql), "DETACH '%s';", attach_name);
-
-        char *err = NULL;
-        if (sqlite3_exec(dsiqs->db, detach_sql, NULL, NULL, &err) != SQLITE_OK) {
-            fprintf(stderr, "Error: Could not detach collection db %s: %s\n", attach_name, err);
-            sqlite3_free(err);
-            /* keep going */
-        }
-    }
-
-    sll_destroy(&dsiqs->attached, free);
-    free(dsiqs);
+    return NULL;
 }
 
 static void dsi_querying_global_exit(void *global) {
@@ -284,6 +166,6 @@ struct plugin_operations gufi_plugin_operations = {
     .ctx_init = dsi_querying_ctx_init,
     .process_dir = NULL,
     .process_file = NULL,
-    .ctx_exit = dsi_querying_ctx_exit,
+    .ctx_exit = NULL,
     .global_exit = dsi_querying_global_exit,
 };
